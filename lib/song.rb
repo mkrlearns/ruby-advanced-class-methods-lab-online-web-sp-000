596f806a7848d16ec89ed9c8f71b5af75cb10697
ryan m <contact@ryanmeek.com>
2019-03-11 00:21:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,4 +31,14 @@ class Song
   def self.find_by_name(name)
     self.all.find{|song| song.name == name}
   end
+
+  def self.find_or_create_by_name(name)
+    self.all.each do |song|
+      if song.name == name
+        name
+      else
+        self.create_by_name(name)
+      end
+    end
+  end
 end
