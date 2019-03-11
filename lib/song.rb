f847de17152391703351f54d3dfae9f3ca89c0bb
ryan m <contact@ryanmeek.com>
2019-03-11 00:12:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,13 +16,13 @@ class Song
     song
   end
 
-  def new_by_name(name)
+  def self.new_by_name(name)
     song = self.new
     song.name = name
     song
   end
   
-  def create_by_name(name)
+  def self.create_by_name(name)
     song = self.create
     song.name = name
     song
